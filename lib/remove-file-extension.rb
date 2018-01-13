07d32a973fd8b37548ba6982ebfe2affe959d26b
Josh Davenport <josh.davenport@tghp.co.uk>
2018-01-13 23:18:59 +0000

    Corrected plugin structure

--- lib/remove-file-extension.rb
+++ lib/remove-file-extension.rb
@@ -1,10 +1,12 @@
 require 'liquid'
 
-# Remove file extension from string
-module RemoveFileExtension
-  def remove_file_extension(str)
-    return str.sub(/\.[A-Za-z]*$/, '')
-  end
+module Jekyll
+	# Remove file extension from string
+	module RemoveFileExtension
+	  def remove_file_extension(str)
+	    return str.sub(/\.[A-Za-z]*$/, '')
+	  end
+	end
 end
 
-Liquid::Template.register_filter(RemoveFileExtension)
+Liquid::Template.register_filter(Jekyll::RemoveFileExtension)
