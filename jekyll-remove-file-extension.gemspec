f149836b9ef0925e212c013be4d49bdde6ac4d84
Josh Davenport <josh.davenport@tghp.co.uk>
2018-01-14 00:34:34 +0000

    Minor updates

--- jekyll-remove-file-extension.gemspec
+++ jekyll-remove-file-extension.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |spec|
   spec.name          = "jekyll-remove-file-extension"
-  spec.version       = '0.1.0'
+  spec.version       = '0.2.1'
   spec.authors       = ["Josh Davenport"]
   spec.email         = ["josh@joshdavenport.co.uk"]
   spec.summary       = 'Super simple jekyll plugin that removes file extensions from a string'
--- lib/remove-file-extension.rb
+++ lib/remove-file-extension.rb
@@ -1,12 +1,12 @@
-require 'liquid'
+require "jekyll"
+require "liquid"
 
 module Jekyll
-	# Remove file extension from string
-	module RemoveFileExtension
-	  def remove_file_extension(str)
-	    return str.sub(/\.[A-Za-z]*$/, '')
-	  end
-	end
+  module RemoveFileExtension
+    def remove_file_extension(str)
+      return str.sub(/\.[A-Za-z]*$/, '')
+    end
+  end
 end
 
 Liquid::Template.register_filter(Jekyll::RemoveFileExtension)
